9f30e707f9ba350bbb006d2b784e6a192d451b83
gleafy <greenleafy@yandex.ru>
2025-11-21 03:17:48 +0400

    update library routes

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, LoanViewSet, UserRegistrationViewSet
+from .views import BookViewSet, LoanViewSet, UserRegistrationViewSet, AuthorViewSet
 
 router = DefaultRouter()
 router.register(r"books", BookViewSet, basename="book")
+router.register(r'authors', AuthorViewSet, basename='author')
 router.register(r"loans", LoanViewSet, basename="loan")
 router.register(r"register", UserRegistrationViewSet, basename="register")
 
