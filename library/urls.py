6d50f5b83925ca6213c249918d2ff97a9d496416
gleafy <greenleafy@yandex.ru>
2025-11-22 00:39:44 +0400

    update library app urls with search endpoint

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, LoanViewSet, AuthorViewSet
+from .views import GlobalSearchView, BookViewSet, LoanViewSet, AuthorViewSet
 
 router = DefaultRouter()
 router.register(r"books", BookViewSet, basename="book")
 router.register(r'authors', AuthorViewSet, basename='author')
 router.register(r"loans", LoanViewSet, basename="loan")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('search/', GlobalSearchView.as_view(), name='global-search'),
+] + router.urls
